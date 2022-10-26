c2a4f8ca39e941acb00241b92c05104a0b8264a6
theendringer <gustepereira@gmail.com>
2022-10-26 16:30:28 -0300

    vai

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import GlobalStyle from "./styles/global"
+import styled from "styled-components";
+import "react-toastify/dist/ReactToastify.css"
+
+
+const Container = styled.div`
+width: 100%;
+max-width: 800px;
+margin-top: 20px;
+display: flex;
+flex-direction: column;
+align-itens: center;
+gap: 10px;
+`;
+
+const Title = styled.h2``;
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Container>
+        <Title>TEste</Title>
+      </Container>
+      <GlobalStyle/>
+    </>
   );
 }
 
